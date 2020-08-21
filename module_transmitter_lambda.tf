4ad61656432e9a2ec278d4b50da367d17451cfb7
poweld2 <david.powell@ons.gov.uk>
2020-08-21 11:49:27 +0100

    Stand-alone lambda module

--- module_transmitter_lambda.tf
+++
@@ -1,9 +0,0 @@
-module "transmitter-lambda" {
-  source = "github.com/ONSdigital/service-intercomms-transmitter-lambda"
-
-  vpc_config = {
-    subnet_ids = ["XXXXXXXXXX", "XXXXXXXXXX"]
-    security_group_ids = ["XXXXXXXXX"]
-  }
-//  source = "takeon"
-}
