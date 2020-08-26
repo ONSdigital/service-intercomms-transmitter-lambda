03f60cefb295f757dbead4a4001515c5dda7ff5c
poweld2 <david.powell@ons.gov.uk>
2020-08-26 13:56:05 +0100

    path.module added

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@ variable "vpc_config" {
   default     = null
   description = "Provide this to allow your function to access your VPC"
   type = object({
-    security_group_ids = list(string)
+    security_group_ids = string
     subnet_ids         = list(string)
   })
 }
