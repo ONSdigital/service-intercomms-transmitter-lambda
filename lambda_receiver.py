c617c1d7352acf6be0b5cc35ef6e82266a43b62c
Andy Urquhart <35800749+urquha@users.noreply.github.com>
2020-09-03 15:49:55 +0100

    Updated receiver handler

--- lambda_receiver.py
+++ lambda_receiver.py
@@ -1,6 +1,13 @@
 import json
 
-
 def handler(event, context):
-    print("we are logging to cloudwatch")
-    print(event)
+    print("Logging JSON to cloudwatch")
+    try:
+        json = event['Records'][0]['body']
+    except:
+        # This will be hit if the receiver is tested in the console
+        json = event
+    
+    print(json)
+    
+    return json
